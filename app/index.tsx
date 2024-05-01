fa80c90f03a675ae3714142d5408ff8083798e7d
murilothebr <murilothebr>
2024-05-01 19:23:00 -0300

    feat: about done

--- app/index.tsx
+++ app/index.tsx
@@ -1,119 +1,46 @@
-import { View, Text, TextInput, StyleSheet, Alert, ImageBackground } from "react-native";
-import React, { useState } from "react";
-import { Link, useRouter } from "expo-router";
-import FormInput from "@/components/form/FormInput";
-import FormButton from "@/components/form/FormButton";
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 import { Spacing } from "@/consts/spacing";
 import { Colors } from "@/consts/colors";
-import FullScreen from "@/components/containers/FullScreen";
-import { Image } from 'expo-image';
+import HeaderWithTitle from "@/components/headers/Header";
 
 export default function index() {
-  const router = useRouter();
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-
-    if (username != "teste") {
-      Alert.alert(
-        "Usuario não registrado",
-        "Certifique-se de que seu usuario está registrado",
-        [
-          {
-            text: "OK",
-            onPress: () => console.log("OK Pressed"),
-            style: "cancel",
-          },
-        ],
-        { cancelable: false }
-      );
-
-      return;
-    }
-
-    if (password != "123") {
-      Alert.alert(
-        "Senha incorreta",
-        "Certifique-se de que seu senha está correta",
-        [
-          {
-            text: "OK",
-            onPress: () => console.log("OK Pressed"),
-            style: "cancel",
-          },
-        ],
-        { cancelable: false }
-      );
-
-      return;
-    }
-
-    router.push("/home");
-  };
-
-  const blurhash =
-    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
-  const image = { uri: "https://docs.expo.dev/static/images/tutorial/splash.png" };
-
   return (
-    <FullScreen>
+    <View>
+     <HeaderWithTitle title="TopCar" actionSheetOptions={['Cancel', 'About', 'Logout']} HideThisPage={false} />
 
-      <Image
-        source={{ uri: "https://www.lojacarros.com.br/static/images/loja-carros.png" }}
-        style={styles.logo}
-        placeholder={blurhash}
-      />
-
-      <FormInput
-        label="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-
-      <FormInput
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-
-
-
-      <FormButton onPress={handleLogin} title="Login!" />
-
-
-      <Link style={styles.registerLink} href="/register">
-        Novo por aqui? Registre-se!
-      </Link>
-
-    </FullScreen>
+    </ View>  
   );
 }
 
 const styles = StyleSheet.create({
-
-  imageContainer: {
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  logo: {
-    width: 200,
-    height: 150,
-    marginBottom: Spacing.md,
-    alignSelf: "center",
-  },
-  image: {
+  footer: {
     flex: 1,
-    width: '100%',
-    height: 10,
-    backgroundColor: '#0553',
+    justifyContent: 'flex-end',
+    marginBottom: 36,
+    maxWidth: '100%',
+    alignItems: 'center',
+  },
+  changePassword: {
+    color: Colors.primary,
+    borderColor: Colors.primary,
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+    borderRadius: 20,
+    borderWidth: 1, 
+    paddingVertical: 8,
+    paddingHorizontal: 16,
   },
   registerLink: {
     marginTop: Spacing.md,
     fontSize: 12,
+    textAlign: "center"
+  },
+  logo: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: Colors.primary,
     textAlign: "center",
-  }
+  },
 });
